e5308d14fe67aa85ca5dae8a9748c9db1972fee3
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-10-31 21:15:02 +0200

    added custom error messages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './post/post-create/post-create.component';
@@ -11,6 +12,10 @@ import { SignupComponent } from '../../src/app/auth/signup/signup.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
  import { AuthInterceptor } from './auth/auth.interceptor';
+import { ErrorComponent } from './error/error/error.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorInterceptorInterceptor } from './error/error-interceptor.interceptor';
 
 @NgModule({
   declarations: [
@@ -18,15 +23,19 @@ import { AppRoutingModule } from './app-routing.module';
     PostCreateComponent,
     PostDisplayComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,  // added to use forms and methods in fruit-create.component.html
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  {provide: HTTP_INTERCEPTORS,useClass: ErrorInterceptorInterceptor,multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
