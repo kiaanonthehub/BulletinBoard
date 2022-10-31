aaf2c9682c567f3d9fa94348015303cfd4a0a968
Kiaan Maharaj <kiaanmaharaj7@gmail.com>
2022-10-31 21:16:02 +0200

    error fix

--- frontend/src/app/error/error/error.component.ts
+++ frontend/src/app/error/error/error.component.ts
@@ -1,12 +1,12 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog'
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.css']
 })
-export class ErrorComponent implements OnInit {
+export class ErrorComponent {
 
   constructor(@Inject (MAT_DIALOG_DATA) public data: {message:string}) { }
 
